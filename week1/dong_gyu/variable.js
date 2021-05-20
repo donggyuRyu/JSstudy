11979b7c0047012aace795f13e52937f9ed44d06
Ryu <78216411+dongmen5149@users.noreply.github.com>
2021-05-21 01:23:54 +0900

    Add files via upload

---
+++ week1/dong_gyu/variable.js
@@ -0,0 +1,38 @@
+let user = 'Gyu';
+let age = 26;
+let message = "Hello JS";
+alert(message);
+message = "Change method!"
+alert(message)
+
+let hello;
+hello = message;
+alert(hello);
+
+let $ = 1;
+let _ = 2;
+alert($ + _);
+
+
+let apple = "small";
+let Apple = "big";
+alert(apple);
+alert(Apple);
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_GREEN;
+
+let admin;
+let name;
+
+name= "John"
+admin = name;
+
+alert(admin)
+
+
+
