45875345707708321eaebb4c4a5b1044baf7a54d
Ryu <78216411+dongmen5149@users.noreply.github.com>
2021-05-23 02:04:47 +0900

    Add files via upload

---
+++ week1/dong_gyu/number.js
@@ -0,0 +1,31 @@
+let n = 123;
+alert(n/0);
+alert(Infinity);
+alert("문장"/2);
+alert("문자열"/2+6);
+
+const bigInt = 1234567891234597855618498641568489616n;
+alert(bigInt);
+
+let str = "String1";
+let str2 = "string2";
+let phrase = `This is ${str}`;
+alert(phrase);
+
+let name = "dong_gyu";
+alert(`hello, my name is ${name}!`);
+
+let isbigger = 4 > 1;
+alert(isbigger);
+
+let age = null;
+
+let result;
+result = prompt("당신의 이름은?", "홍길동");
+alert(`반갑습니다 ${result}씨!`);
+
+let isman = confirm("당신은 남자입니까?")
+
+alert(isman);
+
+
