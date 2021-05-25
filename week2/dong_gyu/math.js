1759412ea8535a509d20afb6463b58053b79558e
Ryu <78216411+dongmen5149@users.noreply.github.com>
2021-05-26 02:16:14 +0900

    Add files via upload

---
+++ week2/dong_gyu/math.js
@@ -0,0 +1,67 @@
+// alert(2**2);
+// alert(4**(1/2));
+
+// let s = "hello" + "universe";
+// alert(s);
+
+// alert("1"+2);
+// alert(1+"2");
+// alert(2+3+"1");
+// alert(2+"1"+3);
+
+// alert(+true);
+// alert(+0);
+
+// let apples = "2";
+// let oranges = "3";
+// alert(apples+oranges);
+// alert(+apples+ +oranges);
+
+// let num1, num2, num3;
+// num1= 2**3;
+// num2=num1
+// num3=num1
+// alert(num1);
+// alert(num2);
+// alert(num3);
+
+// let n=2;
+// n *= 3+4; // n *= 7과 같은 코드
+
+// let counter = 1;
+// counter++;
+// alert(counter);
+// alert(counter);
+
+// let counter1 =1;
+// ++counter1;
+// alert(counter1);
+// alert(counter1);
+
+// alert("" + 1 + 0); // 10
+// alert("" - 1 + 0); // -1
+// alert(true + false);  //1
+// alert(6 / "3"); //2
+// alert("2" * "3"); //6
+// alert(4 + 5 + "px"); // 9px
+// alert("$" + 4 + 5); // $45
+// alert("4" - 2); // 2
+// alert("4px" - 2); //NAN
+// alert(7 / 0); // infinity
+// alert("  -9  " + 5); //   -9  5
+// alert("  -9  " - 5); // -14
+// alert(null + 1); // 1
+// alert(undefined + 1); // NAN
+// alert(" \t \n" - 2);  //-2
+
+let a = prompt("덧셈할 첫 번째 숫자를 입력해주세요.", 1);
+let b = prompt("덧셈할 두 번째 숫자를 입력해주세요.", 2);
+
+alert(+a + +b); // 12
+
+
+
+
+
+
+
---
+++ week2/dong_gyu/type.js
@@ -0,0 +1,19 @@
+let value = true;
+value = String(value);
+alert(typeof value);
+
+let numstr = "1234";
+numstr = Number(numstr);
+alert(typeof numstr);
+
+let boolean1 = 1;
+boolean1 = Boolean(boolean1);
+alert(boolean1);
+
+let boolean2 = "hello";
+boolean2 = Boolean(boolean2);
+alert(boolean2)
+
+let boolean3 = " ";
+boolean3 = Boolean(boolean3);
+alert(boolean3)
